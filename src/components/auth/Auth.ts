425ef5127cab125d3cb5ed32c93ab242e2b358c4
AJ Clemente <aj.clemente@sootds.com>
2021-06-28 22:52:00 -0400

    Setup routes to interface with AWS Cognito

--- src/components/auth/Auth.ts
+++ src/components/auth/Auth.ts
@@ -1,18 +1,52 @@
 import { Router, Request, Response } from 'express';
-import type { UserSignUp } from '../../types';
+import { CognitoUserPool, CognitoUserAttribute, CognitoUser } from 'amazon-cognito-identity-js';
+import type { UserSignUpRequest, UserConfirmAccountRequest } from './types';
 
 export const authRoute = '/auth';
 export const authRouter = Router();
+
+// AWS User Pool
+const userPool = new CognitoUserPool({
+  UserPoolId: process.env.AWS_COGNITO_USER_POOL_ID,
+  ClientId: process.env.AWS_COGNITO_CLIENT_ID
+});
 
 authRouter.get('/ping', (request: Request, response: Response): void => {
   response.send('pong');
 });
 
+// Sign Up
 authRouter.post('/signup', (request: Request, response: Response): void => {
-  const user: UserSignUp = request.body;
-  response.json({
-    firstName: user.firstName,
-    lastName: user.lastName,
-    email: user.email,
+  const user: UserSignUpRequest = request.body;
+
+  const attributeList = [];
+  const attributeEmail = new CognitoUserAttribute({
+    Name: 'email',
+    Value: user.email
+  });
+  attributeList.push(attributeEmail);
+
+  userPool.signUp(user.user_name, user.password, attributeList, null, (err, result) => {
+    if (err) {
+      response.json({ message: err.message, error: JSON.stringify(err) });
+    }
+    response.json({ user_name: result.user.getUsername() });
+  });
+});
+
+// Confirm Account
+authRouter.post('/confirm-account', (request: Request, response: Response): void => {
+  const user: UserConfirmAccountRequest = request.body;
+
+  const cognitoUser = new CognitoUser({
+    Username: user.user_name,
+    Pool: userPool
+  });
+
+  cognitoUser.confirmRegistration(user.confirmation_code, true, (err, result) => {
+    if (err) {
+      response.json({ message: err.message, error: JSON.stringify(err) });
+    }
+    response.json({ message: JSON.stringify(result) });
   });
 });
