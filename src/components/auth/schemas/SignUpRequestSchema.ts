c58b2cfcfcad8d048afed0d76452975abe813c67
AJ Clemente <aj.clemente@sootds.com>
2021-06-29 21:49:23 -0400

    Add `.empty()` validation check to schema

--- src/components/auth/schemas/SignUpRequestSchema.ts
+++ src/components/auth/schemas/SignUpRequestSchema.ts
@@ -1,26 +1,30 @@
 import Joi from 'joi';
 
 export const SignUpRequestSchema = Joi.object().keys({
-  user_name: Joi.string().alphanum().min(3).max(30).required().messages({
+  user_name: Joi.string().alphanum().min(3).max(30).empty().required().messages({
     'string.alphanum': '`user_name` must be alpanumeric.',
     'string.min': '`user_name` length must be 3 or greater.',
     'string.max': '`user_name` length must be 30 or less.',
+    'string.empty': '`user_name` cannot be empty.',
     'any.required': '`user_name` is required.'
   }),
-  first_name: Joi.string().alphanum().min(3).max(30).required().messages({
+  first_name: Joi.string().alphanum().min(3).max(30).empty().required().messages({
     'string.alphanum': '`first_name` must be alpanumeric.',
     'string.min': '`first_name` length must be 3 or greater.',
     'string.max': '`first_name` length must be 30 or less.',
+    'string.empty': '`first_name` cannot be empty.',
     'any.required': '`first_name` is required.'
   }),
-  last_name: Joi.string().alphanum().min(3).max(30).required().messages({
+  last_name: Joi.string().alphanum().min(3).max(30).empty().required().messages({
     'string.alphanum': '`last_name` must be alpanumeric.',
     'string.min': '`last_name` length must be 3 or greater.',
     'string.max': '`last_name` length must be 30 or less.',
+    'string.empty': '`last_name` cannot be empty.',
     'any.required': '`last_name` is required.'
   }),
-  email: Joi.string().email().required().messages({
+  email: Joi.string().email().empty().required().messages({
     'string.email': '`email` must be a valid email.',
+    'string.empty': '`email` cannot be empty.',
     'any.required': '`email` is required.'
   }),
   password: Joi.string()
@@ -29,10 +33,12 @@ export const SignUpRequestSchema = Joi.object().keys({
         /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\^$*.\[\]{}\(\)?\-“!@#%&\/,><\’:;|_~`])\S{8,99}$/
       )
     )
+    .empty()
     .required()
     .messages({
       'string.pattern.base':
         '`password` length must be 8 or greater and must contain a number, a special character, an uppercase and lowercase letter.',
+      'string.empty': '`password` cannot be empty.',
       'any.required': '`password` is required.'
     })
 });
