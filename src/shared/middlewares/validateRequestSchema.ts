70c1c729514da4a39e6324db44b9607ff00a6e57
AJ Clemente <aj.clemente@sootds.com>
2021-06-30 17:23:51 -0400

    Add `: void`

--- src/shared/middlewares/validateRequestSchema.ts
+++ src/shared/middlewares/validateRequestSchema.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import { Schema } from 'joi';
 
 export const validateRequestSchema =
-  (schema: Schema) => (request: Request, response: Response, next: NextFunction) => {
+  (schema: Schema) => (request: Request, response: Response, next: NextFunction): void => {
     const validate = schema.validate(request.body);
     if (validate.error == null) {
       next();
