0080c6348249fa6b28592d52fe3451734786b6a0
AJ Clemente <aj.clemente@sootds.com>
2021-06-30 00:11:43 -0400

    Add `.empty` to schema

--- src/components/auth/schemas/ConfirmAccountRequestSchema.ts
+++ src/components/auth/schemas/ConfirmAccountRequestSchema.ts
@@ -1,13 +1,15 @@
 import Joi from 'joi';
 
 export const ConfirmAccountRequestSchema = Joi.object().keys({
-  user_name: Joi.string().alphanum().min(3).max(30).required().messages({
+  user_name: Joi.string().alphanum().min(3).max(30).empty().required().messages({
     'string.alphanum': "`user_name` must be alpanumeric.",
     'string.min': "`user_name` length must be 3 or greater.",
     'string.max': "`user_name` length must be 30 or less.",
+    'string.empty': '`user_name` cannot be empty.',
     'any.required': "`user_name` is required."
   }),
-  confirmation_code: Joi.string().required().messages({
+  confirmation_code: Joi.string().empty().required().messages({
+    'string.empty': '`confirmation_code` cannot be empty.',
     'any.required': "`confirmation_code` is required."
   })
 });
