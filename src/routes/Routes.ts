8e99a55920a9bf5a0fe8ace156d8b407bea162f8
AJ Clemente <aj.clemente@sootds.com>
2021-07-11 13:33:46 -0400

    Rename directory temporarily

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -2,7 +2,7 @@
 import { Router } from 'express';
 
 // LOCAL IMPORTS
-import { authRoute, authRouter } from '../components/Auth';
+import { authRoute, authRouter } from '../components/test';
 
 export const apiRoute = '/api';
 export const apiRouter = Router();
