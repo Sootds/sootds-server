863ea743b84ab30833388760ecb5cd22073c8d77
AJ Clemente <aj.clemente@sootds.com>
2021-08-03 19:23:11 -0400

    Add main Stores API endpoints
    - `vendors/stores/{store_id}` GET endpoint
    - `vendors/stores` POST endpoint
    - `vendors/stores/{store_id}` PUT endpoint
    - `vendors/stores/{store_id}` DELETE endpoint

--- src/components/Vendors/components/Stores/Stores.ts
+++ src/components/Vendors/components/Stores/Stores.ts
@@ -12,3 +12,31 @@ export const storesRouter = Router();
 storesRouter.get('/ping', (_: Request, response: Response): void => {
   response.status(200).json({ message: 'pong', timestamp: Date.now() });
 });
+
+// Get Store By ID
+storesRouter.get('/:store_id', (request: Request, response: Response): void => {
+  response
+    .status(200)
+    .json({ message: `Store ID: ${request.params.store_id}`, timestamp: Date.now() });
+});
+
+// Create New Store
+storesRouter.post('/', (request: Request, response: Response): void => {
+  response
+    .status(200)
+    .json({ message: `New store created.`, timestamp: Date.now() });
+});
+
+// Update Store
+storesRouter.put('/:store_id', (request: Request, response: Response): void => {
+  response
+    .status(200)
+    .json({ message: `Update store ${request.params.store_id}.`, timestamp: Date.now() });
+});
+
+// Delete Store
+storesRouter.delete('/:store_id', (request: Request, response: Response): void => {
+  response
+    .status(200)
+    .json({ message: `Delete store ${request.params.store_id}.`, timestamp: Date.now() });
+});
