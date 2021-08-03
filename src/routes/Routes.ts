495aca9b5a5c8ecf6c2ff5842aa5361ca3f2a6b4
AJ Clemente <aj.clemente@sootds.com>
2021-08-03 19:09:02 -0400

    Add Vendors component to API routes

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -3,8 +3,10 @@ import { Router } from 'express';
 
 // LOCAL IMPORTS
 import { authRoute, authRouter } from '../components/Auth';
+import { vendorsRoute, vendorsRouter } from '../components/Vendors';
 
 export const apiRoute = '/api';
 export const apiRouter = Router();
 
 apiRouter.use(authRoute, authRouter);
+apiRouter.use(vendorsRoute, vendorsRouter);
