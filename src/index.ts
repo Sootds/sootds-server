9c6e7eebfc84bbe20f2e1bc4f49dfaf676329249
AJ Clemente <aj.clemente@sootds.com>
2021-08-08 10:59:14 -0400

    Split `server` from `.listen` call for testing

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,5 @@
-// EXTERNAL IMPORTS
-import express, { RequestHandler } from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-
 // LOCAL IMPORTS
-import { apiRoute, apiRouter } from './routes';
-
-// Create Express server.
-const server = express();
-const port = process.env.PORT || 4000;
-
-// Setup middleware.
-server.use(cors({ origin: true, credentials: true }));
-server.use(express.urlencoded({ extended: true }) as RequestHandler);
-server.use(express.json() as RequestHandler);
-server.use(cookieParser());
-
-// Setup API router.
-server.use(apiRoute, apiRouter);
+import { server, port } from './server';
 
 server.listen(port, (): void => {
   if (process.env.NODE_ENV == 'development') {
