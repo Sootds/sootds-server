3f99bc8b1dc9ec8d144c893acab13c9bcdacb075
AJ Clemente <aj.clemente@sootds.com>
2021-08-07 20:17:36 -0400

    Add main Users API endpoints

--- src/routes/Routes.ts
+++ src/routes/Routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 
 // LOCAL IMPORTS
 import { authRoute, authRouter } from '../components/Auth';
+import { usersRoute, usersRouter } from '../components/Users';
 import { vendorsRoute, vendorsRouter } from '../components/Vendors';
 
 // Setup main API router.
@@ -11,4 +12,5 @@ export const apiRouter = Router();
 
 // Hook up components.
 apiRouter.use(authRoute, authRouter);
+apiRouter.use(usersRoute, usersRouter);
 apiRouter.use(vendorsRoute, vendorsRouter);
