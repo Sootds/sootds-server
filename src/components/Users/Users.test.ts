fee43c1b63d825d0a01b7a490b4a7e88e9206d14
AJ Clemente <aj.clemente@sootds.com>
2021-08-08 12:21:17 -0400

    Create unit test for Users API

---
+++ src/components/Users/Users.test.ts
@@ -0,0 +1,33 @@
+// EXTERNAL IMPORTS
+import request from 'supertest';
+
+// LOCAL IMPORTS
+import { server } from '../../server';
+
+describe('/api/users/', (): void => {
+  describe('GET /ping', (): void => {
+    const statusCode = 200;
+    it(`should return a status code of '${statusCode}'`, async (): Promise<void> => {
+      const response = await request(server).get('/api/users/ping');
+      expect(response.statusCode).toEqual(200);
+    });
+  });
+
+  describe('GET /:username', (): void => {
+    const username = 'testing_username';
+    const message = `Successfully retrieved user ${username}.`;
+    it(`should return a message '${message}'`, async (): Promise<void> => {
+      const response = await request(server).get(`/api/users/${username}`);
+      expect(response.body.message).toEqual(message);
+    });
+  });
+
+  describe('PUT /:username', (): void => {
+    const username = 'testing_username';
+    const message = `Update user ${username}.`;
+    it(`should return a message '${message}'`, async (): Promise<void> => {
+      const response = await request(server).put(`/api/users/${username}`);
+      expect(response.body.message).toEqual(message);
+    });
+  });
+});
